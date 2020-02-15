2b7c175b407a9d4804f5899ded2974bf2cec6989
Guilherme Viana <ggviana@hotmail.com.br>
2020-02-15 12:56:41 -0700

    Updates

---
+++ precache-manifest.f7ff79ea1d9fc11ae5cb9b149c5d94b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98803148dc3a7517b3790746062d9c9d",
+    "url": "/option-deployer/index.html"
+  },
+  {
+    "revision": "b251dd26ed04a8055bfb",
+    "url": "/option-deployer/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b251dd26ed04a8055bfb",
+    "url": "/option-deployer/static/js/2.ab2e40d2.chunk.js"
+  },
+  {
+    "revision": "325d65e89ba744894d35f4b8d171c195",
+    "url": "/option-deployer/static/js/2.ab2e40d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14ff71ad9511a5be7347",
+    "url": "/option-deployer/static/js/main.1bde7258.chunk.js"
+  },
+  {
+    "revision": "3775e42f66199fcdd631",
+    "url": "/option-deployer/static/js/runtime-main.38984f49.js"
+  },
+  {
+    "revision": "cc8307fe4e1ea470ff6ab837ec06992a",
+    "url": "/option-deployer/static/media/logo.cc8307fe.svg"
+  }
+]);
